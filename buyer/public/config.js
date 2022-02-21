7b27872c4dfab288159cb7637ded5905fd724930
majing5 <majing5@tuniu.com>
2022-02-21 16:09:44 +0800

    修改接口地址

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,15 +3,15 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      common: "https://common-api.noctisma.com",
+      buyer: "https://buyer-api.noctisma.com",
+      seller: "https://store-api.noctisma.com",
+      manager: "https://admin-api.noctisma.com"
     },
     API_PROD: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      common: "https://common-api.noctisma.com",
+      buyer: "https://buyer-api.noctisma.com",
+      seller: "https://store-api.noctisma.com",
+      manager: "https://admin-api.noctisma.com"
     },
   };
--- seller/public/config.js
+++ seller/public/config.js
@@ -3,25 +3,25 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "https://common-api.noctisma.com",
+    buyer: "https://buyer-api.noctisma.com",
+    seller: "https://store-api.noctisma.com",
+    manager: "https://admin-api.noctisma.com"
   },
   API_PROD: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "https://common-api.noctisma.com",
+    buyer: "https://buyer-api.noctisma.com",
+    seller: "https://store-api.noctisma.com",
+    manager: "https://admin-api.noctisma.com"
   },
   /**
    * @description // 跳转买家端地址 pc端
    */
-  PC_URL: "https://pc-b2b2c.pickmall.cn",
+  PC_URL: "https://pc-b2b2c.noctisma.com",
   /**
    * @description  // 跳转买家端地址 wap端
    */
-  WAP_URL: "https://m-b2b2c.pickmall.cn",
+  WAP_URL: "https://m-b2b2c.noctisma.com",
   /**
    *  @description api请求基础路径前缀
    */
